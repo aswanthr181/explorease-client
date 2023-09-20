16f98a513dbd20b49e2d286740935636c3c65237
aswanth <aswanthr18tech@gmail.com>
2023-09-20 10:32:18 +0530

    url changes

--- src/Constants/Api.jsx
+++ src/Constants/Api.jsx
@@ -1,3 +1,3 @@
-export const userAPI = "https://api.explorease.site/";
-export const adminAPI = "https://api.explorease.site/admin/";
-export const agencyAPI="https://api.explorease.site/agency/"
+export const userAPI = "https://api.explorease.site";
+export const adminAPI = "https://api.explorease.site/admin";
+export const agencyAPI="https://api.explorease.site/agency"
