c73de9d8979e1e04ceb77707dba5b1ddc6bcb06f
aswanth <aswanthr18tech@gmail.com>
2023-09-21 20:53:18 +0530

    constan

--- src/Constants/Api.jsx
+++ src/Constants/Api.jsx
@@ -1,6 +1,5 @@
 export const userAPI = "https://api.explorease.site";
-// export const adminAPI = "https://api.explorease.site/admin";
-// export const agencyAPI="https://api.explorease.site/agency"
-export const agencyAPI="http://localhost:3000/agency/"
-export const adminAPI = "http://localhost:3000/admin/";
+export const adminAPI = "https://api.explorease.site/admin";
+export const agencyAPI="https://api.explorease.site/agency"
+
 
